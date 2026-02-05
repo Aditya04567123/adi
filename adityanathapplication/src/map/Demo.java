fed60f8cc8252c59314581a9532d3c08f3d18374
ADITYA <ADITYA@LAPTOP-R7PM12TC>
2026-02-05 15:25:50 +0530

    till Demo

---
+++ adityanathapplication/src/map/Demo.java
@@ -0,0 +1,40 @@
+package map;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		 HashMap hm=new HashMap();
+		 hm.put(100,99);
+		 hm.put(50,999);
+		 hm.put(150,9999);
+		 hm.put(25,99999);
+		 hm.put(75,999999);
+		 hm.put(125,9999999);
+		 System.out.println(hm);
+		 System.out.println("============");
+		 Set s=hm.keySet();
+		 Iterator itr=s.iterator();
+		 while(itr.hasNext()) {
+			 System.out.println(itr.next()+",");
+		 }
+		 Collection c=hm.values(); 
+		 System.out.println("values");
+		 Iterator itr1=c.iterator();
+		 System.out.println();
+		 while(itr1.hasNext()) {
+			 System.out.println(itr1.next());
+		 }
+		 Set s1=hm.entrySet();
+		 System.out.println("entry set");
+		 Iterator itr2=s1.iterator();
+		 while(itr2.hasNext()) {
+			 System.out.println(itr2.next());
+		 }
+		 
+	}
+}
